b1049b897b2890e9ba50c97d9ae35a3a83750038
Zach Byers <byerszach@gmail.com>
2018-01-30 23:57:40 -0700

    ui and prime checking complete

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  func isPrime(_ number: Int) -> Bool {
+    return number > 1 && !(2..<number).contains { number % $0 == 0 }
+  }
 
+  @IBOutlet weak var primeField: UITextField!
+  
+  @IBOutlet weak var primeOutput: UILabel!
+  
+  @IBAction func primeButton(_ sender: Any) {
+    if let input = Int(primeField.text!) {
+      if isPrime(input) {
+        primeOutput.text = "Yep, \(input) is a prime number!"
+        primeField.text = ""
+      } else {
+        primeOutput.text = "Ooh, nope, \(input) is not prime."
+        primeField.text = ""
+      }
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
